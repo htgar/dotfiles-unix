aff886aa0df89ec9e43d736561c5daec643bc968
Htgar <development@htgar.org>
2024-03-11 10:26:31 +0800

    Replaced devbox with homebrew

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,8 +2,14 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-devbox global shellenv --init-hook | source
-starship init fish | source
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+if test -d (brew --prefix)"/share/fish/completions"
+    set -p fish_complete_path (brew --prefix)/share/fish/completions
+end
+
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
 
 set -g fish_greeting
 set -gx EDITOR nvim
